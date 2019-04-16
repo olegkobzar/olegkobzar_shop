7f80cb4100ec320e0ae8fdc90efc18c0ee2814e0
Oleg Kobzar <olegkobzar2209@gmail.com>
2019-04-16 19:17:27 +0300

    start project react

--- src/tests/users.js
+++
@@ -1,7 +0,0 @@
-export const getUsers = () => {
-  const url = 'https://jsonplaceholder.typicode.com/users';
-  fetch(url)
-    .then(data => data.json())
-    .then(console.log)
-    .catch(console.error);
-};
