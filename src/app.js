5d1cd1e0b530a4fdd1e10b33e88efc3ca8924e53
Oleg Kobzar <olegkobzar2209@gmail.com>
2019-04-21 10:45:30 +0300

    change variable

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import './index.scss';
+import './app.scss';
 
 import { Header } from './components/header';
 import { Footer } from './components/footer';
