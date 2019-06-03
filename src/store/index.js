318e43e88284c5754a079d24d9130cfa6a0b7edc
Oleg Kobzar <olegkobzar2209@gmail.com>
2019-06-03 23:11:47 +0300

    add class work

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,10 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 import { user } from './user';
 import { products, product } from './products';
 import { categories, category, info } from './categories';
 import { status } from './status';
+import { rootSaga } from './sagas';
 
 const rootReducers = combineReducers({
   user,
@@ -19,3 +21,21 @@ export const store = createStore(
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() // eslint-disable-line
 );
 // second argument is used to add redux dev tool (should be pre-installed in browser)
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const sagaMiddleware = createSagaMiddleware();
+
+const rootReducers = combineReducers({
+  user,
+  products,
+  product,
+  categories,
+  category,
+  info,
+  status
+});
+
+export const store = createStore(
+  rootReducers,
+  composeEnhancers(applyMiddleware(sagaMiddleware))
+);
